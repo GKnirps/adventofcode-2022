5c754ab2f6d05b334ac1a89f626b57348321b8c9
Guido Knips <guido.knips@posteo.de>
2022-12-18 18:02:44 +0100

    Add solution for day 18, part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -13,6 +13,9 @@ fn main() -> Result<(), String> {
     let a = surface_area(&cubes);
     println!("The surface area of the lava droplets is {a}");
 
+    let b = reachable_surface_area(&cubes);
+    println!("The exterior surface area is {b}");
+
     Ok(())
 }
 
@@ -67,6 +70,48 @@ fn surface_area(cubes: &HashSet<P>) -> u32 {
         .sum::<u32>()
 }
 
+fn reachable_surface_area(cubes: &HashSet<P>) -> u32 {
+    // also a primitive approach, but it should be doable
+    let min_x = cubes.iter().map(|(x, _, _)| x).min().copied().unwrap_or(1) - 1;
+    let min_y = cubes.iter().map(|(_, y, _)| y).min().copied().unwrap_or(1) - 1;
+    let min_z = cubes.iter().map(|(_, _, z)| z).min().copied().unwrap_or(1) - 1;
+    let max_x = cubes.iter().map(|(x, _, _)| x).max().copied().unwrap_or(-1) + 1;
+    let max_y = cubes.iter().map(|(_, y, _)| y).max().copied().unwrap_or(-1) + 1;
+    let max_z = cubes.iter().map(|(_, _, z)| z).max().copied().unwrap_or(-1) + 1;
+
+    let max_space = ((max_x as isize - min_x as isize + 1)
+        * (max_y as isize - min_y as isize + 1)
+        * (max_z as isize - min_z as isize + 1)) as usize;
+    let mut flooded: HashSet<P> = HashSet::with_capacity(max_space);
+    let mut surface_area: u32 = 0;
+    let mut stack: Vec<P> = Vec::with_capacity(max_space);
+    stack.push((min_x, min_y, min_z));
+
+    while let Some(p) = stack.pop() {
+        if cubes.contains(&p) {
+            surface_area += 1;
+            continue;
+        }
+        if flooded.contains(&p) {
+            continue;
+        }
+        flooded.insert(p);
+        for (x, y, z) in neighbours(p) {
+            if x >= min_x
+                && x <= max_x
+                && y >= min_y
+                && y <= max_y
+                && z >= min_z
+                && z <= max_z
+                && !flooded.contains(&(x, y, z))
+            {
+                stack.push((x, y, z));
+            }
+        }
+    }
+    surface_area
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -96,5 +141,17 @@ mod test {
 
         // then
         assert_eq!(a, 64);
+    }
+
+    #[test]
+    fn reachable_surface_area_works_for_example() {
+        // given
+        let cubes = parse_input(EXAMPLE).expect("expected successful parsing");
+
+        // when
+        let a = reachable_surface_area(&cubes);
+
+        // then
+        assert_eq!(a, 58);
     }
 }
